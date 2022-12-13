53218539780f24364b05f65b2e4fa0cd6d8e5aa7
olumide-ajayi <dajayi02@gmail.com>
2022-12-12 17:02:13 -0800

    task 7 and 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - initializing stage
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
